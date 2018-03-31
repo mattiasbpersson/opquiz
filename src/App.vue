11ad8880366a075e12e74abae8fa071a50f14c28
mattiasbpersson <mattiasbpersson@gmail.com>
2018-04-01 00:57:52 +0200

    Moved question rendering to helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,11 @@
 <template>
     <div id="app">
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>Question</th>
-                <th>Alternatives</th>
-                <th>Answer</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(question) in questions" :key="question.question">
-                <td>{{question['.key']}}</td>
-                <td>{{question.alternatives}}</td>
-                <td>{{question.answer}}</td>
-            </tr>
-            </tbody>
-        </table>
         <img src="./assets/logo.png">
         <router-view/>
     </div>
 </template>
 
 <script>
-import {db} from './firebase';
-
-export default {
-  firebase: {
-    questions: db.ref('questions')
-  }
-}
 </script>
 
 <style>
