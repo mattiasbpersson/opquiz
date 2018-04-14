21f9d7db5481e5c224fdb6a296eabc7a73ffdf0a
mattiasbpersson <mattiasbpersson@gmail.com>
2018-04-14 10:40:28 +0200

    Edit inline på questions, men utan validering

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,90 +1,128 @@
 <template>
     <div id="app">
         <p><router-link to="/add-question">AddQuestion</router-link></p>
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>Question</th>
-                <th>Alternatives</th>
-                <th>Answer</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(question) in questions"
-                class='question'
-                :key="question['.key']">
-                <td>
-                <template v-if="question != editedQuestion">
-                    <div class="view" @click="editQuestion(question)">{{question.question}}</div>
-                </template>
-                <template v-if="question == editedQuestion">
-                <input class="edit" type="text"
-                    v-model="question.question"
-                    v-question-focus="question == editedQuestion"
-                    @blur="doneEdit(question)"
-                    @keyup.enter="doneEdit(question)"
-                    @keyup.esc="cancelEdit(question)">
-                </template>
-                </td>
-                <td>{{question.alternatives}}</td>
-                <td>{{question.answer}}</td>
-            </tr>
-            </tbody>
-        </table>
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-btn color="primary" dark slot="activator" class="mb-2">New Question</v-btn>
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ formTitle }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Question" v-model="editedItem.question"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Alternativs" v-model="editedItem.alternatives"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Answer" v-model="editedItem.answer"></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat @click.native="close">Cancel</v-btn>
+          <v-btn flat @click.native="save">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-data-table
+      :headers="headers"
+      :items="questions"
+      hide-actions
+      class="elevation-1"
+    >
+      <template slot="items" slot-scope="props">
+        <td class="text-xs-left">{{ props.item.question }}</td>
+        <td class="text-xs-left">{{ props.item.alternatives }}</td>
+        <td class="text-xs-left">{{ props.item.answer }}</td>
+        <td class="justify-center layout px-0">
+          <v-btn icon class="mx-0" @click="editItem(props.item)">
+            <v-icon color="teal">edit</v-icon>
+          </v-btn>
+          <v-btn icon class="mx-0" @click="deleteItem(props.item)">
+            <v-icon color="pink">delete</v-icon>
+          </v-btn>
+        </td>
+      </template>
+      <template slot="no-data">
+        <v-alert :value="true" color="error" icon="warning">
+           No questions found
+        </v-alert>
+      </template>
+    </v-data-table>
     </div>
 </template>
 
 <script>
-import { db } from '@/firebase'
+import { db } from '@/firebase';
 
 export default {
   name: 'Questions',
   firebase: {
     questions: db.ref('questions')
   },
-  data () {
+  data() {
     return {
-      editedQuestion: null
-    }
+      editedQuestion: null,
+      dialog: false,
+      headers: [
+        { text: 'Question', value: 'question' },
+        { text: 'Alternatives', value: 'alternatives' },
+        { text: 'Answer', value: 'answer' }
+      ],
+      editedKey: '',
+      editedItem: {
+        question: '',
+        alternatives: '',
+        answer: ''
+      },
+      defaultItem: {
+        question: '',
+        alternatives: '',
+        answer: ''
+      }
+    };
   },
   methods: {
-    editQuestion: function (question) {
-      console.log(question.question)
-      this.beforeEditCache = question
-      this.editedQuestion = question
+    editItem(question) {
+      this.editedKey = question['.key'];
+      console.log('EditItem: ' + this.editedKey);
+      this.editedItem = Object.assign({}, question);
+      this.dialog = true;
     },
-    doneEdit: function (question) {
-      if (!this.editedQuestion) {
-        return
+    deleteItem(question) {
+      const index = this.questions.indexOf(question);
+      confirm('Are you sure you want to delete this item?') &&
+        this.$firebaseRefs.questions.child(question['.key']).remove();
+    },
+    close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedKey = -1;
+      }, 300);
+    },
+    save() {
+      console.log('Save: ' + this.editedKey);
+      delete this.editedItem['.key']
+      if (this.editedKey != '') {
+        this.$firebaseRefs.questions.child(this.editedKey).set(this.editedItem)
+      } else {
+        this.$firebaseRefs.questions.push(this.editedItem);
       }
-      this.editedQuestion = null
-      question.question = question.question.trim()
-      if (!question.question) {
-        // this.removeQuestion(question);
-      }
-      this.saveEdit(question)
-    },
-    saveEdit: function (question) {
-      // create a copy of the item
-      const copy = { ...question }
-      // remove the .key attribute
-      delete copy['.key']
-      this.$firebaseRefs.questions.child(question['.key']).set(copy)
-      // questions.child(item[".key"]).set(copy);
-    },
-    cancelEdit: function (question) {
-      this.editedQuestion = null
-      question = this.beforeEditCache
+      this.close();
     }
   },
-  directives: {
-    'question-focus': function (el, binding) {
-      if (binding.value) {
-        el.focus()
-      }
+  computed: {
+    formTitle() {
+      return this.editedKey === '' ? 'New Item' : 'Edit Item';
     }
   }
-}
+};
 </script>
 <style scoped>
 [v-cloak] {
